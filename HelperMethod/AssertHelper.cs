b33b1f63747ac52e153a33d38e82da7fd73f1a84
pnghia211 <pnghia211@gmail.com>
2025-06-22 20:23:32 +0700

    update

--- HelperMethod/AssertHelper.cs
+++ HelperMethod/AssertHelper.cs
@@ -13,8 +13,8 @@ class Program
     {
         var filePath = @"D:\porter\learning\DynamicItinerary\TestData\Culture_enUS.html";
 
-        var expectedflightNumbers = new List<string> { "Ottawa, ON (YOW)", "PD 2205" };
-        var selectors = new List<string> { "//span[contains(text(),'Arrival time')]/ancestor::table[1]//p[contains(text(), 'Ottawa')]", $"//p[normalize-space()='PD 2205']" };
+        // var expectedflightNumbers = new List<string> { "Ottawa, ON (YOW)", "PD 2205" };
+        // var selectors = new List<string> { "//span[contains(text(),'Arrival time')]/ancestor::table[1]//p[contains(text(), 'Ottawa')]", $"//p[normalize-space()='PD 2205']" };
 
         // AssertHelper.assertHTML(filePath, selectors, expectedflightNumbers);
         var selectors1 = new CultureENUSSelector();
@@ -23,6 +23,34 @@ class Program
 
     public static class AssertHelper
     {
+        public static List<string> ExtractTextFromSelectors<T>(string filePath, T selectorClass) where T : ISelectorProvider
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+                throw new ArgumentException("File path is null or empty.");
+
+            var doc = new HtmlDocument();
+            doc.Load(filePath);
+
+            var results = new List<string>();
+
+            var selectors = selectorClass.getAllSelectors();
+
+            foreach (var xpath in selectors)
+            {
+                var nodes = doc.DocumentNode.SelectNodes(xpath);
+                if (nodes != null)
+                {
+                    foreach (var node in nodes)
+                    {
+                        results.Add(node.InnerText.Trim());
+
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(",", results));
+            return results;
+        }
+
         public static void assertHTML(string filePath, List<string> xpaths, List<string> expectedResult)
         {
             var doc = new HtmlDocument();
@@ -64,33 +92,7 @@ class Program
         }
 
 
-        public static List<string> ExtractTextFromSelectors<T>(string filePath, T selectorClass) where T : ISelectorProvider
-        {
-            if (string.IsNullOrWhiteSpace(filePath))
-                throw new ArgumentException("File path is null or empty.");
-
-            var doc = new HtmlDocument();
-            doc.Load(filePath);
-
-            var results = new List<string>();
-
-            var selectors = selectorClass.getAllSelectors();
-
-            foreach (var xpath in selectors)
-            {
-                var nodes = doc.DocumentNode.SelectNodes(xpath);
-                if (nodes != null)
-                {
-                    foreach (var node in nodes)
-                    {
-                        results.Add(node.InnerText.Trim());
-
-                    }
-                }
-            }
-            Console.WriteLine(string.Join(",", results));
-            return results;
-        }
+        
     }
 }
 
